eb6aa87a0dd7e680c061833c27e43ed05ca7d1d5
abrarradan30 <abradan84@gmail.com>
2025-04-26 13:13:45 +0700

    [ADD] package auth

--- be-sports/routes/api.php
+++ be-sports/routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
-Route::apiResource('/posts', App\Http\Controllers\Api\UserController::class);
+Route::apiResource('/posts', App\Http\Controllers\Api\UsersController::class);
