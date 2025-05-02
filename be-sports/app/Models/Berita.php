429bc0cfff3a06a1ff538608e13bb5062b29f6da
ghoniWe <ghonigaming7@gmail.com>
2025-05-02 09:22:45 +0700

    add controller

--- be-sports/app/Models/Berita.php
+++ be-sports/app/Models/Berita.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Berita extends Model
@@ -17,4 +18,11 @@ class Berita extends Model
         'gambar_sampul',
         'tgl_dibuat',
     ];
+
+    protected function gambarSampul(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($gambar_sampul) => url('/storage/berita/' . $$gambar_sampul),
+        );
+    }
 }
