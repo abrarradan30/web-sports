d347f9ed8c6d9835e539992a5cf480ec3ea853d8
Rozin <rozin123098@gmail.com>
2025-05-02 09:42:51 +0700

    Fix: Resolve merge conflicts in routes/api.php and update controller names

--- be-sports/routes/api.php
+++ be-sports/routes/api.php
@@ -3,6 +3,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UsersController;
 use App\Http\Controllers\Api\GaleriController;
+use App\Http\Controllers\Api\KategoriOlahragaController;
+use App\Http\Controllers\Api\ProdukOlahragaController;
+use App\Http\Controllers\Api\BeritaController;
+use App\Http\Controllers\Api\KontakController;
 
 Route::post('/login', [UsersController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
@@ -10,5 +14,16 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [UsersController::class, 'logout']);
 });
 
-
 Route::apiResource('/galeri', GaleriController::class);
+Route::get('/galeri/{id}/edit', [GaleriController::class, 'edit']);
+Route::put('/galeri/{id}', [GaleriController::class, 'update']);
+Route::delete('/galeri/{id}', [GaleriController::class, 'destroy']);
+
+Route::apiResource('kategori-olahraga', KategoriOlahragaController::class);
+
+Route::apiResource('produk-olahraga', ProdukOlahragaController::class);
+
+Route::apiResource('berita', BeritaController::class);
+
+Route::apiResource('kontak', KontakController::class);
+
