169df9087f62e9d91b57f54bc518e548822319f8
ghoniWe <ghonigaming7@gmail.com>
2025-05-06 15:22:11 +0700

    update

--- be-sports/app/Models/JenisOlahraga.php
+++ be-sports/app/Models/JenisOlahraga.php
@@ -5,17 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-<<<<<<< HEAD
+
 class JenisOlahraga extends Model 
-{
-    use HasFactory;
     
-=======
-class KategoriOlahraga extends Model
 {
     use HasFactory;
 
->>>>>>> bff8b2e3290e9841cf2b540a4d68247efae57636
     protected $table = 'jenis_olahraga';
 
     protected $fillable = [
